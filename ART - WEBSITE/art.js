0dfa117c302056ff001b7bb0bcfc43d877412f8f
juan <baezrjuan@gmail.com>
2020-12-17 20:53:36 -0500

    update

--- ART - WEBSITE/art.js
+++ ART - WEBSITE/art.js
@@ -4,11 +4,11 @@ function exitOverview() {
 }
 
 function submit() {
+    sessionStorage.title = $("#title_text").val();
     if (!sessionStorage.title) {
         $("#title_msg").html('<b style="color:red; background-color: rgb(0,0,0,.6);">INSERT A TITLE!</b>');
         return;
     }
-    sessionStorage.title = $("#title_text").val();
     console.log("TITLE", sessionStorage.title);
 
     if (!sessionStorage.thumbnails) {
@@ -17,6 +17,10 @@ function submit() {
     }
     sessionStorage.thumbnails = $("#thumbnails_container").html();
     //console.log("THUMBNAILS", sessionStorage.thumbnails);
+    if (!sessionStorage.bgroundimg) {
+        $("#file_bground_msg").html('<b style="color:red; background-color: rgb(0,0,0,.6);">UPLOAD PIC!</b>');
+        return;
+    }
     console.log("BACKGROUND IMG", sessionStorage.bgroundimg);
     console.log("FONT", sessionStorage.font);
     window.open("portfolio.html");
